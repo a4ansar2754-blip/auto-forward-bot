3a885c3b7ed59c2fcb417ac5f89c44b46e9c72c9
a4ansar2754-blip <a4ansar2754@gmail.com>
2026-03-14 20:35:59 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,32 +1,75 @@
+import asyncio
 from telethon import events
 from userbot_manager import clients
 from config_manager import get_config
 
-def start_engine(user_id):
+album_cache = {}
 
-    client = clients[user_id]
+def start_engine(user):
+
+    client = clients[user]
 
     @client.on(events.NewMessage)
     async def forward(event):
 
-        data = get_config(user_id)
+        data = get_config(user)
 
         if str(event.chat_id) not in data["sources"]:
             return
 
+        text = event.raw_text or ""
+
+        # block links
+        if "http" in text:
+            return
+
+        reply_map = {}
+
         for target in data["targets"]:
 
-            if event.media:
+            if event.grouped_id:
+
+                album_cache.setdefault(event.grouped_id,[]).append(event)
+
+                await asyncio.sleep(1)
+
+                msgs = album_cache.pop(event.grouped_id,[])
+
+                files = [m.media for m in msgs]
 
                 await client.send_file(
                     int(target),
-                    event.media,
-                    caption=event.text
+                    files,
+                    caption=text
                 )
+
+            elif event.media:
+
+                sent = await client.send_file(
+                    int(target),
+                    event.media,
+                    caption=text
+                )
+
+                reply_map[event.id] = sent.id
 
             else:
 
-                await client.send_message(
+                sent = await client.send_message(
                     int(target),
-                    event.text
+                    text
                 )
+
+                reply_map[event.id] = sent.id
+
+            # auto delete
+            asyncio.create_task(auto_delete(client,target,sent.id))
+
+async def auto_delete(client,chat,msg):
+
+    await asyncio.sleep(60)
+
+    try:
+        await client.delete_messages(int(chat),msg)
+    except:
+        pass
