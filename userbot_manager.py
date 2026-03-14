084df2f5b26f90509a66c76909bbd03ccaa6921f
a4ansar2754-blip <a4ansar2754@gmail.com>
2026-03-14 21:58:08 +0530

    Update userbot_manager.py

--- userbot_manager.py
+++ userbot_manager.py
@@ -1,11 +1,13 @@
-from telethon import TelegramClient
 import os
+from telethon import TelegramClient
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 
 clients = {}
 phone_hash = {}
+
+os.makedirs("sessions", exist_ok=True)
 
 
 async def login_user(user_id, phone=None, code=None):
@@ -16,21 +18,20 @@ async def login_user(user_id, phone=None, code=None):
 
     await client.connect()
 
-    # STEP 1 SEND OTP
+    # STEP 1 → SEND OTP
     if phone:
 
         r = await client.send_code_request(phone)
 
         phone_hash[user_id] = r.phone_code_hash
-
         clients[user_id] = client
 
         return "CODE"
 
-    # STEP 2 VERIFY OTP
+    # STEP 2 → VERIFY OTP
     if code:
 
-        client = clients[user_id]
+        client = clients.get(user_id)
 
         await client.sign_in(
             code=code,
