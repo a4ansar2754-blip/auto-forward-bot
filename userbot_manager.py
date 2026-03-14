01ca1b0c78871f3ca3c974cc7d6565cd6e8f0635
a4ansar2754-blip <a4ansar2754@gmail.com>
2026-03-14 22:39:15 +0530

    Update userbot_manager.py

--- userbot_manager.py
+++ userbot_manager.py
@@ -4,38 +4,49 @@ from telethon import TelegramClient
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 
-clients = {}
-phone_hash = {}
-
+# session folder
 os.makedirs("sessions", exist_ok=True)
 
+clients = {}
+phone_code_hash = {}
 
 async def login_user(user_id, phone=None, code=None):
 
     session_file = f"sessions/{user_id}"
 
-    client = TelegramClient(session_file, API_ID, API_HASH)
+    if user_id not in clients:
+        client = TelegramClient(session_file, API_ID, API_HASH)
+        await client.connect()
+        clients[user_id] = client
 
-    await client.connect()
+    client = clients[user_id]
 
-    # STEP 1 → SEND OTP
+    # STEP 1 SEND OTP
     if phone:
 
-        r = await client.send_code_request(phone)
+        result = await client.send_code_request(phone)
 
-        phone_hash[user_id] = r.phone_code_hash
-        clients[user_id] = client
+        phone_code_hash[user_id] = result.phone_code_hash
+        clients[user_id].phone = phone
 
         return "CODE"
 
-    # STEP 2 → VERIFY OTP
+    # STEP 2 VERIFY OTP
     if code:
 
-        client = clients.get(user_id)
+        try:
 
-        await client.sign_in(
-            code=code,
-            phone_code_hash=phone_hash[user_id]
-        )
+            await client.sign_in(
+                phone=client.phone,
+                code=code,
+                phone_code_hash=phone_code_hash[user_id]
+            )
 
-        return "SUCCESS"
+            return "SUCCESS"
+
+        except Exception as e:
+
+            print(e)
+            return "FAIL"
+
+    return "FAIL"
